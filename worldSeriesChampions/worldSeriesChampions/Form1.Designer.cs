d34eb25d92fc50491e546d027a82cfa716143b90
Antravaya <120424177+Antravaya@users.noreply.github.com>
2023-05-19 23:39:01 -0400

    Add files via upload

---
+++ worldSeriesChampions/worldSeriesChampions/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace worldSeriesChampions
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.countriesListBox = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select one of the following teams:";
+            // 
+            // countriesListBox
+            // 
+            this.countriesListBox.FormattingEnabled = true;
+            this.countriesListBox.ItemHeight = 15;
+            this.countriesListBox.Location = new System.Drawing.Point(12, 27);
+            this.countriesListBox.Name = "countriesListBox";
+            this.countriesListBox.Size = new System.Drawing.Size(261, 124);
+            this.countriesListBox.TabIndex = 3;
+            this.countriesListBox.SelectedIndexChanged += new System.EventHandler(this.countriesListBox_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(12, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(261, 115);
+            this.label3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 307);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.countriesListBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "World Series Champions ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox countriesListBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ worldSeriesChampions/worldSeriesChampions/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace worldSeriesChampions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> teamsList = new List<string>();
+        List<int> yearList = new List<int>();
+        int i = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string countryName;
+            StreamReader inputFile;
+            StreamReader anotherInputFile;
+            inputFile = File.OpenText("Teams.txt");
+            anotherInputFile = File.OpenText("WorldSeriesWinners.txt");
+            
+            while (!inputFile.EndOfStream)
+            {
+                countryName = inputFile.ReadLine();
+                countriesListBox.Items.Add(countryName);
+            }
+
+
+            string temp;
+            temp = anotherInputFile.ReadLine();
+            
+            while(temp != null)
+            {
+                teamsList.Add(temp);
+                temp = anotherInputFile.ReadLine();
+                i++;
+            }
+        }
+
+        private void countriesListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string teamNames = countriesListBox.SelectedItem.ToString();
+            int num = 0;
+
+            foreach (string anotherInputFile in teamsList)
+            {
+                if (anotherInputFile == teamNames)
+                {
+                    num++;
+                }
+            }
+
+            label3.Text = teamNames + " has won " + num + " times from " + " through ";
+        }
+    }
+}
