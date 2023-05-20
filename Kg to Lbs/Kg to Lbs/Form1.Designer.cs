30c470e64e5dba0679ab50965758ea0948192b48
Antravaya <120424177+Antravaya@users.noreply.github.com>
2023-05-19 23:34:06 -0400

    Add files via upload

---
+++ Kg to Lbs/Kg to Lbs/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace Kg_to_Lbs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Kg = new System.Windows.Forms.Label();
+            this.Lbs = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Kg
+            // 
+            this.Kg.AutoSize = true;
+            this.Kg.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.Kg.Location = new System.Drawing.Point(29, 26);
+            this.Kg.Name = "Kg";
+            this.Kg.Size = new System.Drawing.Size(20, 13);
+            this.Kg.TabIndex = 0;
+            this.Kg.Text = "Kg";
+            this.Kg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Lbs
+            // 
+            this.Lbs.AutoSize = true;
+            this.Lbs.Location = new System.Drawing.Point(29, 69);
+            this.Lbs.Name = "Lbs";
+            this.Lbs.Size = new System.Drawing.Size(24, 13);
+            this.Lbs.TabIndex = 1;
+            this.Lbs.Text = "Lbs";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(59, 66);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(153, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // result
+            // 
+            this.result.BackColor = System.Drawing.SystemColors.Info;
+            this.result.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.result.Location = new System.Drawing.Point(32, 106);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(193, 70);
+            this.result.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "kg to lbs";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(90, 191);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "lbs to kg";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(171, 191);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 7;
+            this.ExitButton.Text = "E&xit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(258, 241);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Lbs);
+            this.Controls.Add(this.Kg);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Kg;
+        private System.Windows.Forms.Label Lbs;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ Kg to Lbs/Kg to Lbs/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kg_to_Lbs
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Kg_to_Lbs()
+        {
+            int kg = Convert.ToInt32(textBox1.Text);
+
+            double lbs = Convert.ToDouble(kg * 2.2);
+
+            result.Text = "Your result is " + lbs;
+        }
+
+        public void Lbs_to_Kg()
+        {
+            int lbs = Convert.ToInt32(textBox2.Text);
+
+            double kg = Convert.ToDouble(lbs / 2.2);
+
+            result.Text = "Your result is " + Math.Round(kg, 2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Kg_to_Lbs();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Lbs_to_Kg();
+        }
+    }
+}
