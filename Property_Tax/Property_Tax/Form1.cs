870ad2603e5b2af792d8312c9268a74a93d3f18a
Antravaya <120424177+Antravaya@users.noreply.github.com>
2024-03-04 19:53:58 -0500

    Add files via upload

---
+++ Property_Tax/Property_Tax/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Property_Tax
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            property_Tax();
+        }
+
+        public void property_Tax()
+        {
+            double result;
+            int propertyTax = int.Parse(textBox1.Text);
+            result = propertyTax / 100 * 0.64;
+            label2.Text = "Your Sales tax is " + result.ToString("c");
+        }
+    }
+}
