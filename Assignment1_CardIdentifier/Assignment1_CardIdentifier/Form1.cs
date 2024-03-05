0bf3c71f301d47bbbda3b43c74051114da45d88d
Antravaya <120424177+Antravaya@users.noreply.github.com>
2024-03-04 19:52:35 -0500

    Add files via upload

---
+++ Assignment1_CardIdentifier/Assignment1_CardIdentifier/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment1_CardIdentifier
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            label2.Text = "This is a Joker card.";
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "This is a Jack of Diamond.";
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            label2.Text = "This is a Ace of Hearts.";
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            label2.Text = "This is a Six of Clubs";
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            label2.Text = "This is a Two of Spades";
+        }
+    }
+}
